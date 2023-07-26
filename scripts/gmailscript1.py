8e37b6df78529fcf4e7e0a0283d3ad737331d6fe
Austin <49465593+Xenohex@users.noreply.github.com>
2023-07-26 19:02:30 -0400

    fixes and changes since old website bad

--- scripts/gmailscript1.py
+++ scripts/gmailscript1.py
@@ -1,33 +1,45 @@
 import webbrowser
 import time
-import win32com.client
+import os
+import sys
+import pyautogui as pag
 
-shell = win32com.client.Dispatch("WScript.Shell")
 
 url = "gmail.com"
 email_to = "ajpliska873t.i@gmail.com"
 subject = "test script"
-msg = "This is a message that was totally not send by a python script.\nteehee"
+msg = "This is a message that was totally not send by a python script."
 
 # accessing webpage
 webbrowser.open("url")
-time.sleep(9)
+time.sleep(1)
 
-# the actual emailing part
-shell.SendKeys("c", 0)
-time.sleep(1)
-shell.SendKeys("email_to", 0)
-time.sleep(1)
-shell.SendKeys("{TAB}", 0)
-time.sleep(1)
-shell.SendKeys("{TAB}", 0)
-time.sleep(1)
-shell.SendKeys("subject", 0)
-time.sleep(1)
-shell.SendKeys("{TAB}", 0)
-time.sleep(1)
-shell.SendKeys("msg", 0)
-time.sleep(1)
-shell.SendKeys("{TAB}", 0)
-time.sleep(1)
-shell.SendKeys("{ENTER}", 0)
+for i in range(3):
+      pag.moveTo(100, 100, duration=0.25)
+      pag.moveTo(200, 100, duration=0.25)
+      pag.moveTo(200, 200, duration=0.25)
+      pag.moveTo(100, 200, duration=0.25)
+
+pag.keyDown("w")
+pag.keyUp("w")
+
+pag.press("s")
+
+# # the actual emailing part
+# shell.SendKeys("c", 0)
+# time.sleep(1)
+# shell.SendKeys("email_to", 0)
+# time.sleep(1)
+# shell.SendKeys("{TAB}", 0)
+# time.sleep(1)
+# shell.SendKeys("{TAB}", 0)
+# time.sleep(1)
+# shell.SendKeys("subject", 0)
+# time.sleep(1)
+# shell.SendKeys("{TAB}", 0)
+# time.sleep(1)
+# shell.SendKeys("msg", 0)
+# time.sleep(1)
+# shell.SendKeys("{TAB}", 0)
+# time.sleep(1)
+# shell.SendKeys("{ENTER}", 0)
