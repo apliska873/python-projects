9fb13b10f2dce6fdd1d1438d815a2dd32c990e0e
Austin <49465593+Xenohex@users.noreply.github.com>
2023-07-26 21:35:57 -0400

    alterations but nothing done yet

--- scripts/gmailscript1.py
+++ scripts/gmailscript1.py
@@ -4,42 +4,21 @@ import os
 import sys
 import pyautogui as pag
 
-
-url = "gmail.com"
-email_to = "ajpliska873t.i@gmail.com"
-subject = "test script"
-msg = "This is a message that was totally not send by a python script."
-
-# accessing webpage
-webbrowser.open("url")
-time.sleep(1)
-
-for i in range(3):
+# mouse movement
+"""for i in range(3):
       pag.moveTo(100, 100, duration=0.25)
       pag.moveTo(200, 100, duration=0.25)
       pag.moveTo(200, 200, duration=0.25)
-      pag.moveTo(100, 200, duration=0.25)
+      pag.moveTo(100, 200, duration=0.25)"""
 
+print("sleeping")
+time.sleep(5)
+
+print("pressing W")
 pag.keyDown("w")
+time.sleep(5)
 pag.keyUp("w")
-
-pag.press("s")
-
-# # the actual emailing part
-# shell.SendKeys("c", 0)
-# time.sleep(1)
-# shell.SendKeys("email_to", 0)
-# time.sleep(1)
-# shell.SendKeys("{TAB}", 0)
-# time.sleep(1)
-# shell.SendKeys("{TAB}", 0)
-# time.sleep(1)
-# shell.SendKeys("subject", 0)
-# time.sleep(1)
-# shell.SendKeys("{TAB}", 0)
-# time.sleep(1)
-# shell.SendKeys("msg", 0)
-# time.sleep(1)
-# shell.SendKeys("{TAB}", 0)
-# time.sleep(1)
-# shell.SendKeys("{ENTER}", 0)
+print("pressing S")
+pag.keyDown("s")
+time.sleep(5)
+pag.keyUp("s")
